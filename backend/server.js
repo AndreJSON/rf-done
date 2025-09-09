319852fff23fc20e559ef7d7f4a53e01a0a728e2
André Goude <andre.v.json@gmail.com>
2025-09-09 19:09:52 +0200

    Security update.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,16 @@ app.use(express.json());
 app.use(express.urlencoded({
 	extended: true
 }));
+const helmet = require("helmet");
+app.use(helmet());
+
+app.use((err, req, res, next) => {
+  if (err instanceof URIError) {
+    console.warn("Blocked suspicious request:", req.url);
+    return res.status(400).send("Bad Request");
+  }
+  next(err);
+});
 
 require('./paths.js')(app);
 require('./routes.js')(app);
